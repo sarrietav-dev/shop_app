269ee6b4d5b83de6b8d435adf99fdbc331f43a90
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-22 14:16:58 -0500

    minor(check_status): make checkStatus protected

--- lib/http/check_status.dart
+++ lib/http/check_status.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
 mixin StatusChecker {
+  @protected
   void checkStatus(Response response) {
     if (response.statusCode >= 400) throw HttpException("Something went wrong");
   }

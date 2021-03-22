4ee27d2fba239c7df22a0b032cd864533cc3dc4b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-22 14:16:28 -0500

    feat: create class that manages orders http requests

---
+++ lib/http/orders_http_handler.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:shop_app/http/check_status.dart';
+import 'package:shop_app/http/http_request_handler.dart';
+import 'package:shop_app/http/url_handler.dart';
+
+class OrdersHttpHandler extends HTTPRequestHandler with StatusChecker {
+  OrdersHttpHandler({body, resourceId})
+      : super(body: body, resourceId: resourceId) {
+    urlHandler = URLHandler(collectionName: "orders");
+  }
+
+  @override
+  Future<http.Response> fetchData() async {
+    final response = await http.get(urlHandler.url);
+    checkStatus(response);
+    return response;
+  }
+
+  Future<http.Response> addOrder() async {
+    checkBody();
+    final response = await http.post(urlHandler.url, body: json.encode(body));
+    checkStatus(response);
+    return response;
+  }
+
+  Future<http.Response> clear() async {
+    final response = await http.patch(urlHandler.url, body: json.encode({}));
+    checkStatus(response);
+    return response;
+  }
+}

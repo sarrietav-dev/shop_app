aba97a67d14f15675c9b6339bc258d3aa2ee86b3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-21 23:51:12 -0500

    refactor(http_request_handler): change from abstract to interface

--- lib/http/http_request_handler.dart
+++ lib/http/http_request_handler.dart
@@ -1,9 +1,7 @@
+import 'package:http/http.dart';
 import 'package:shop_app/http/url_handler.dart';
 
-abstract class HTTPRequestHandler {
-  final URLHandler urlHandler;
-
-  HTTPRequestHandler(this.urlHandler);
-
-  Future<void> fetchData();
+class HTTPRequestHandler {
+  URLHandler urlHandler;
+  Future<Response> fetchData() {}
 }

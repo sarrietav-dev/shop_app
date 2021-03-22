893318c5af43cd87995cfc00bd8b4a5c9049ec88
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-22 00:36:41 -0500

    feat: create class that handles http request for cart

---
+++ lib/http/cart_http_handler.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:shop_app/http/check_status.dart';
+import 'package:shop_app/http/http_request_handler.dart';
+import 'package:shop_app/http/url_handler.dart';
+
+class CartHttpHandler extends HTTPRequestHandler with StatusChecker {
+  @override
+  URLHandler urlHandler;
+  final String resourceId;
+  final dynamic body;
+
+  CartHttpHandler({this.resourceId, this.body})
+      : super(resourceId: resourceId, body: body) {
+    urlHandler = URLHandler(collectionName: "cart");
+  }
+
+  @override
+  Future<http.Response> fetchData() async {
+    final response = await http.get(urlHandler.url);
+    checkStatus(response);
+    return response;
+  }
+
+  Future<http.Response> removeItem() async {
+    checkResourceId();
+    final response = await http.delete(urlHandler.getResourceUrl(resourceId));
+    checkStatus(response);
+    return response;
+  }
+
+  Future<http.Response> update() async {
+    checkBody();
+    final response = await http.patch(urlHandler.url, body: json.encode(body));
+    checkStatus(response);
+    return response;
+  }
+
+  Future<http.Response> clear() async {
+    final response = await http.delete(urlHandler.url);
+    checkStatus(response);
+    return response;
+  }
+}

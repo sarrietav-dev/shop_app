2ebc18cca25e157532abc00db6523bb78fb7e0b0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-21 23:52:54 -0500

    feat(product_listing_handler): create a class that handles http request for product_listing

---
+++ lib/http/product_listing_handler.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:shop_app/http/http_request_handler.dart';
+import 'package:shop_app/http/url_handler.dart';
+import 'package:http/http.dart' as http;
+
+class ProductListingHTTPHandler implements HTTPRequestHandler {
+  URLHandler urlHandler;
+  final String resourceId;
+  final dynamic body;
+
+  ProductListingHTTPHandler({this.body, this.resourceId}) {
+    urlHandler = URLHandler(collectionName: "products");
+  }
+
+  @override
+  Future<http.Response> fetchData() async {
+    return await http.get(urlHandler.url);
+  }
+
+  Future<http.Response> addProduct() async {
+    _checkBody();
+    return http.post(urlHandler.url, body: json.encode(body));
+  }
+
+  Future<http.Response> updateProduct() async {
+    _checkResourceId();
+    _checkBody();
+    return await http.patch(urlHandler.getResourceUrl(resourceId),
+        body: json.encode(body));
+  }
+
+  Future<http.Response> deleteProduct() async {
+    _checkResourceId();
+    return await http.delete(urlHandler.getResourceUrl(resourceId));
+  }
+
+  void _checkResourceId() {
+    if (resourceId == null) throw ArgumentError.notNull("resourceID");
+  }
+
+  void _checkBody() {
+    if (body == null) throw ArgumentError.notNull("body");
+  }
+}

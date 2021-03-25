e931501d4922b27ff3abb8416fe6c5535fe2e719
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-25 18:19:22 -0500

    feat(auth_url_handler): Use firebase api key as an enviroment variable

--- lib/http/url_handlers/auth_url_handler.dart
+++ lib/http/url_handlers/auth_url_handler.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class AuthUrlHandler {
-  static const baseUrl = "identitytoolkit.googleapis.com";
+  static final baseUrl =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${String.fromEnvironment('FIREPABE_API_KEY')}";
 
   AuthUrlHandler({@required collectionName});
 }

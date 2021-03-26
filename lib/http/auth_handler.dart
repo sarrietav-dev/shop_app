77c87c2c4aa9e62f34bf9638a57a6b6e4511a063
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-25 23:48:27 -0500

    refactor: move and rename auth_url_handler to http/

--- lib/http/auth_handler.dart
+++ lib/http/auth_handler.dart
@@ -3,14 +3,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shop_app/http/check_status.dart';
 
-class AuthUrlHandler with StatusChecker {
+class AuthHandler with StatusChecker {
   final url = Uri.https("identitytoolkit.googleapis.com",
       "/v1/accounts:signInWithCustomToken?key=${String.fromEnvironment('FIREPABE_API_KEY')}");
 
   final String username;
   final String password;
 
-  AuthUrlHandler({this.username, this.password});
+  AuthHandler({this.username, this.password});
 
   Future<http.Response> signup() async {
     return await http.post(url, body: json.encode(jsonValues));

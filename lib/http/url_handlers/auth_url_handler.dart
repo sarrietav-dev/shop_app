bb04b8866f4a19ae9ed33ce02ec6b1d0bc3f87a4
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-25 23:42:35 -0500

    feat(auth_url_handler): create AuthUrlHandler attributes and methods

--- lib/http/url_handlers/auth_url_handler.dart
+++ lib/http/url_handlers/auth_url_handler.dart
@@ -1,9 +1,20 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:shop_app/http/check_status.dart';
 
-class AuthUrlHandler {
-  static final baseUrl =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${String.fromEnvironment('FIREPABE_API_KEY')}";
+class AuthUrlHandler with StatusChecker {
+  final url = Uri.https("identitytoolkit.googleapis.com",
+      "/v1/accounts:signInWithCustomToken?key=${String.fromEnvironment('FIREPABE_API_KEY')}");
 
-  AuthUrlHandler({@required collectionName});
+  final String username;
+  final String password;
+
+  AuthUrlHandler({this.username, this.password});
+
+  Future<http.Response> signup() async {
+    return await http.post(url, body: json.encode(jsonValues));
+  }
+
+  get jsonValues => {"username": username, "password": password};
 }

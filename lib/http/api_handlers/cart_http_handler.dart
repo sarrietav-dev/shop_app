7c8406c4be4e9e6818afe60e2d08e4012bec4428
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-28 01:06:02 -0500

    fix(cart_http_handler): import error

--- lib/http/api_handlers/cart_http_handler.dart
+++ lib/http/api_handlers/cart_http_handler.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:shop_app/http/check_status.dart';
-import 'package:shop_app/http/api/http_request_handler.dart';
+import 'package:shop_app/http/api_handlers/http_request_handler.dart';
 import 'package:shop_app/http/url_handlers/api_url_handler.dart';
 
 class CartHttpHandler extends HTTPRequestHandler with StatusChecker {

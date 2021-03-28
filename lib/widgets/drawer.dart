eb773d2588f7ac950269a72cc4f74005242bd944
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-03-28 13:43:06 -0500

    feat(drawer): add logout button to the end of the drawer

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -7,38 +7,54 @@ class MainDrawer extends StatelessWidget {
     return Drawer(
       child: Column(
         children: [
-          Container(
-              height: 150,
-              width: double.infinity,
-              padding: const EdgeInsets.all(10),
-              color: Theme.of(context).accentColor,
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Shopping Center",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: Colors.white),
-              )),
-          SizedBox(
+          const _DrawerBanner(),
+          const SizedBox(
             height: 20,
           ),
           ListTile(
-            leading: Icon(Icons.home),
-            title: Text("Home page"),
+            leading: const Icon(Icons.home),
+            title: const Text("Home page"),
             onTap: () => Navigator.of(context).pushReplacementNamed("/"),
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           ListTile(
-            leading: Icon(Icons.settings),
-            title: Text("Manage Products"),
+            leading: const Icon(Icons.settings),
+            title: const Text("Manage Products"),
             onTap: () => Navigator.of(context)
                 .pushReplacementNamed(ProductManagementPage.routeName),
+          ),
+          const Spacer(),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text("Logout"),
           ),
         ],
       ),
     );
+  }
+}
+
+class _DrawerBanner extends StatelessWidget {
+  const _DrawerBanner({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 150,
+        width: double.infinity,
+        padding: const EdgeInsets.all(10),
+        color: Theme.of(context).accentColor,
+        alignment: Alignment.centerLeft,
+        child: const Text(
+          "Shopping Center",
+          style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: Colors.white),
+        ));
   }
 }

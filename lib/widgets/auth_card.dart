dbfec204ea02eeb87de9994c379018aea679fca9
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-04-17 23:43:44 -0500

    refactor(auth_card): extract exception handling to a mixin

--- lib/widgets/auth_card.dart
+++ lib/widgets/auth_card.dart
@@ -99,7 +99,7 @@ class _AuthCardForm extends StatefulWidget {
   __AuthCardFormState createState() => __AuthCardFormState();
 }
 
-class __AuthCardFormState extends State<_AuthCardForm> {
+class __AuthCardFormState extends State<_AuthCardForm> with _ExceptionHandlers {
   var _isLoading = false;
 
   final GlobalKey<FormState> _formKey = GlobalKey();
@@ -136,52 +136,12 @@ class __AuthCardFormState extends State<_AuthCardForm> {
           break;
       }
     } on HttpException catch (e) {
-      handleHttpException(e);
+      handleHttpException(e, context);
     }
 
     setState(() {
       _isLoading = false;
     });
-  }
-
-  void handleHttpException(HttpException e) => showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-            title: Text("An error ocurred"),
-            content: Text(getErrorContentText(e)),
-            actions: [
-              TextButton(
-                  onPressed: Navigator.of(context).pop, child: Text("Ok")),
-            ],
-          ));
-
-  String getErrorContentText(HttpException e) {
-    switch (e.message) {
-      case "EMAIL_EXISTS":
-        return "The email address is already in use by another account.";
-        break;
-      case "OPERATION_NOT_ALLOWED":
-        return "Password sign-in is disabled for this project.";
-        break;
-      case "TOO_MANY_ATTEMPTS_TRY_LATER":
-        return "We have blocked all requests from this device due to unusual activity. Try again later.";
-        break;
-      case "EMAIL_NOT_FOUND":
-        return "There is no user record corresponding to this identifier. The user may have been deleted.";
-        break;
-      case "INVALID_PASSWORD":
-        return " The password is invalid or the user does not have a password.";
-        break;
-      case "USER_DISABLED":
-        return "The user account has been disabled by an administrator.";
-        break;
-      case "WEAK_PASSWORD":
-        return "The password must be 6 characters long or more";
-        break;
-      default:
-        return "Authentication has failed";
-        break;
-    }
   }
 
   void _switchAuthMode() {
@@ -285,5 +245,47 @@ class __AuthCardFormState extends State<_AuthCardForm> {
         ),
       ),
     );
+  }
+}
+
+mixin _ExceptionHandlers {
+  void handleHttpException(HttpException e, BuildContext context) => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: Text("An error ocurred"),
+            content: Text(getErrorContentText(e)),
+            actions: [
+              TextButton(
+                  onPressed: Navigator.of(context).pop, child: Text("Ok")),
+            ],
+          ));
+
+  String getErrorContentText(HttpException e) {
+    switch (e.message) {
+      case "EMAIL_EXISTS":
+        return "The email address is already in use by another account.";
+        break;
+      case "OPERATION_NOT_ALLOWED":
+        return "Password sign-in is disabled for this project.";
+        break;
+      case "TOO_MANY_ATTEMPTS_TRY_LATER":
+        return "We have blocked all requests from this device due to unusual activity. Try again later.";
+        break;
+      case "EMAIL_NOT_FOUND":
+        return "There is no user record corresponding to this identifier. The user may have been deleted.";
+        break;
+      case "INVALID_PASSWORD":
+        return " The password is invalid or the user does not have a password.";
+        break;
+      case "USER_DISABLED":
+        return "The user account has been disabled by an administrator.";
+        break;
+      case "WEAK_PASSWORD":
+        return "The password must be 6 characters long or more";
+        break;
+      default:
+        return "Authentication has failed";
+        break;
+    }
   }
 }
